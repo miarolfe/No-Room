e1208d8d3511e0556a65e930e45f339c26dd1e09
Peter Rolfe <30848697+pjhrolfe@users.noreply.github.com>
2023-09-30 16:17:55 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <SDL.h>
+
+// Window size
+#define WINDOW_WIDTH 640
+#define WINDOW_HEIGHT 480
+
+int main(int argc, char* args[])
+{
+    // SDL initialisation
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    {
+        SDL_Log("SDL_Init Error: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    // Window creation and position in the center of the screen
+    SDL_Window* window = SDL_CreateWindow("Hello World SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == nullptr)
+    {
+        SDL_Log("SDL_CreateWindow Error: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    // Render creation
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == nullptr)
+    {
+        SDL_Log("SDL_CreateRenderer Error: %s\n", SDL_GetError());
+        return -1;
+    }
+
+    // Draw the texture in the screen
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+
+    // Keep the main loop until the window is closed (SDL_QUIT event)
+    bool exit = false;
+    SDL_Event eventData;
+    while (!exit)
+    {
+        while (SDL_PollEvent(&eventData))
+        {
+            switch (eventData.type)
+            {
+                case SDL_QUIT:
+                    exit = true;
+                    break;
+            }
+        }
+    }
+
+    // Destroy the render, window and finalise SDL
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
